3b1ed9918508bfa2da0a7e9090e0641df8d798cb
nd-the-explorer <81695788+nd-the-explorer@users.noreply.github.com>
2024-03-08 17:08:40 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.prompts import (
@@ -10,6 +10,9 @@ from langchain.prompts import (
 import streamlit as st
 from streamlit_chat import message
 from utils import *
+import os
+from dotenv import load_dotenv
+load_dotenv(r'C:\Users\darji\OneDrive\Desktop\chatbot\.env')
 
 st.header("GTU Mitra- AI based chatbot")
 
@@ -19,14 +22,32 @@ if 'responses' not in st.session_state:
 if 'requests' not in st.session_state:
     st.session_state['requests'] = []
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets['OPENAI_API_KEY'])
-
+llm = ChatOpenAI(model_name=os.getenv('MODEL'), openai_api_key=os.getenv('OPENAI_API_KEY'))
 if 'buffer_memory' not in st.session_state:
-            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+    st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
 
 
-system_msg_template = SystemMessagePromptTemplate.from_template(template="""If user greets you then ggive greetings back, Only answer the question if it is present in the following text,
-                                                                 don't try to generate answer if it is not present in the text. If you don't know the answer then politely say sorry and say that you don't have that information currently""")
+system_msg_template = SystemMessagePromptTemplate.from_template(
+    template=
+    """
+    If user greets you, then greet user back.
+
+    Only answer the question if it is present in the context,
+    don't try to generate answer if it is not present in the context. 
+
+    When processing queries related to specific information which can change over a period of time, 
+    such as name of someone on a specific position or a contact details or etc, 
+    it's essential to consider the datetime information associated with each piece of context chunk to ensure accuracy and relevance.
+
+    1. In the context you are provided with the different chunks each having datetime information and the relevant inforamtion.
+    2. considering the datetime information, Select the part of the context with the latest datetime.
+    3. Based on the selected context chunk, provide the relevant response. 
+
+    If you don't know the answer then politely say sorry and say that you don't have that information currently.
+
+    
+    
+    """)
 
 
 #Prompt 1: Answer the question as truthfully as possible using the provided context,and if the answer is not contained within the text below, say 'I don't know'
@@ -74,4 +95,4 @@ with response_container:
             if i < len(st.session_state['requests']):
                 message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
 
-          
+
